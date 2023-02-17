b39119629dac05c4f4bbd3d95f7ea9282c07a392
cjeon <cjeon@student.42seoul.kr>
2023-02-17 18:28:48 +0900

    direct printk

--- src/printk/printk.rs
+++ src/printk/printk.rs
@@ -1,50 +1,18 @@
-use crate::console::CONSOLE_MANAGER;
+use crate::console::ConsoleManager;
 use core::fmt;
 
-const BUFFER_SIZE: usize = 5;
-
-pub struct DebugMessage {
-	len: usize,
-	pub buf: [u8; BUFFER_SIZE],
-}
-
-impl DebugMessage {
-	const fn new() -> Self {
-		DebugMessage {
-			len: 0,
-			buf: [0; BUFFER_SIZE],
-		}
-	}
-
-	pub unsafe fn flush(&mut self) {
-		CONSOLE_MANAGER.get().dmesg().write_buf(&self.buf, self.len);
-		self.len = 0;
-	}
-
-	fn write_byte(&mut self, b: u8) {
-		self.buf[self.len] = b;
-		self.len += 1;
-		if self.len >= BUFFER_SIZE {
-			unsafe { self.flush() }
-		}
-	}
-}
-
-impl fmt::Write for DebugMessage {
+impl fmt::Write for ConsoleManager {
 	fn write_str(&mut self, s: &str) -> fmt::Result {
-		for b in s.bytes() {
-			self.write_byte(b)
-		}
+		unsafe { self.dmesg().write_buf(s.as_bytes()) }
 		Ok(())
 	}
 
 	fn write_char(&mut self, c: char) -> fmt::Result {
-		self.write_byte(c as u8);
+		let buf = [c as u8];
+		unsafe { self.dmesg().write_buf(&buf) }
 		Ok(())
 	}
 }
-
-pub static mut DMESG: DebugMessage = DebugMessage::new();
 
 #[macro_export]
 macro_rules! printkln {
@@ -52,9 +20,10 @@ macro_rules! printkln {
 		use core::fmt::Write;
 		unsafe {
 			let (res1, res2) = (
-				$crate::printk::DMESG.write_fmt(core::format_args!($($arg)*)),
-				$crate::printk::DMESG.write_char(b'\n' as char),
+				$crate::console::CONSOLE_MANAGER.get().write_fmt(core::format_args!($($arg)*)),
+				$crate::console::CONSOLE_MANAGER.get().write_char(b'\n' as char),
 			);
+
 			if let (Err(_), Err(_)) =  (res1, res2){
 				panic!("printk failed");
 			}
@@ -67,7 +36,7 @@ macro_rules! printk {
 	($($arg:tt)*) => {
 		use core::fmt::Write;
 		unsafe {
-			let res = $crate::printk::DMESG.write_fmt(core::format_args!($($arg)*));
+			let res = $crate::console::CONSOLE_MANAGER.get().write_fmt(core::format_args!($($arg)*));
 			if let Err(_) = res {
 				panic!("printk failed");
 			}
