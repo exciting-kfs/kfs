befa045ab9ce5a5bee6b948995f4e8a2855fde85
cjeon <cjeon@student.42seoul.kr>
2023-04-21 14:51:10 +0900

    ktest: verify attr argument

--- kfs_macro/src/lib.rs
+++ kfs_macro/src/lib.rs
@@ -1,36 +1,11 @@
-extern crate alloc;
-
-use alloc::{format, string::ToString};
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Ident, ItemFn};
-
-const PREFIX: &'static str = "kernel_test";
+use syn::{parse::Nothing, parse_macro_input, ItemFn};
 
 #[proc_macro_attribute]
-pub fn kernel_test(attr: TokenStream, input: TokenStream) -> TokenStream {
-	let mut input_fn = parse_macro_input!(input as ItemFn);
-	let attr = attr.to_string();
+pub fn ktest(attr: TokenStream, input: TokenStream) -> TokenStream {
+	parse_macro_input!(attr as Nothing);
 
-	let prefix = match attr.is_empty() {
-		true => format!("{}_", PREFIX),
-		false => format!("{}_{}_", PREFIX, attr),
-	};
-
-	let new_name = format!("{}{}", prefix, input_fn.sig.ident.to_string());
-	input_fn.sig.ident = Ident::new(&new_name, input_fn.sig.ident.span());
-
-	let expanded = quote! {
-		#[no_mangle]
-		#input_fn
-	};
-
-	let output = TokenStream::from(expanded);
-	output
-}
-
-#[proc_macro_attribute]
-pub fn ktest(_: TokenStream, input: TokenStream) -> TokenStream {
 	let test_func = parse_macro_input!(input as ItemFn);
 
 	let ident = &test_func.sig.ident;
