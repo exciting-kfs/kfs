669190204cc6e05c99f3b12760547a28654b6995
42mypark <skyttl34@gmail.com>
2023-05-02 13:29:20 +0900

    style

--- src/mm/slub/cache_manager/no_alloc_list.rs
+++ src/mm/slub/cache_manager/no_alloc_list.rs
@@ -11,30 +11,30 @@ pub struct Node<T> {
 }
 
 impl<T> Node<T> {
-        pub const NODE_SIZE: usize = mem::size_of::<Node<T>>();
-        
-        /// Construct a Node<T> for memory chunk
-        /// 
-        /// # Safety
-        /// 
-        /// * The size of memory chunk must be bigger than Node::NODE_SIZE
-        pub unsafe fn construct_at(mem: &mut [u8], data: T) -> &mut Self {
-                let ptr = mem.as_mut_ptr() as *mut Self;
-                let next = NonNull::new_unchecked(&mut (*ptr));
-                let prev = next.clone();
-                (*ptr) = Node { prev, next, data };
-                &mut (*ptr)
+	pub const NODE_SIZE: usize = mem::size_of::<Node<T>>();
+
+	/// Construct a Node<T> for memory chunk
+	/// 
+	/// # Safety
+	/// 
+	/// * The size of memory chunk must be bigger than Node::NODE_SIZE
+	pub unsafe fn construct_at(mem: &mut [u8], data: T) -> &mut Self {
+		let ptr = mem.as_mut_ptr() as *mut Self;
+		let next = NonNull::new_unchecked(&mut (*ptr));
+		let prev = next.clone();
+		(*ptr) = Node { prev, next, data };
+		&mut (*ptr)
 	}
 
-        pub fn data(&self) -> &T {
-                &self.data
-        }
+	pub fn data(&self) -> &T {
+		&self.data
+	}
 
-        pub fn data_mut(&mut self) -> &mut T {
-                &mut self.data
-        }
+	pub fn data_mut(&mut self) -> &mut T {
+		&mut self.data
+	}
 
-        #[inline(always)]
+	#[inline(always)]
 	pub fn as_non_null(&mut self) -> NonNull<Node<T>> {
 		unsafe { NonNull::new_unchecked(self as *mut Node<T>) }
 	}
@@ -45,31 +45,31 @@ impl<T> Node<T> {
 	}
 
 	#[inline(always)]
-        pub fn as_mut_ptr(&mut self) -> *mut Self {
-                self as *mut Self
-        }
+	pub fn as_mut_ptr(&mut self) -> *mut Self {
+		self as *mut Self
+	}
 
-        #[inline(always)]
-        pub fn as_ptr(&self) -> *const Self {
-                self as *const Self
-        }
+	#[inline(always)]
+	pub fn as_ptr(&self) -> *const Self {
+		self as *const Self
+	}
 
 	pub fn disjoint(&mut self) {
-                let prev = unsafe { self.prev.as_mut() };
-                let next = unsafe { self.next.as_mut() };
+		let prev = unsafe { self.prev.as_mut() };
+		let next = unsafe { self.next.as_mut() };
 
-                prev.next = self.next;
-                next.prev = self.prev;
+		prev.next = self.next;
+		next.prev = self.prev;
 
-                self.next = self.as_non_null();
-                self.prev = self.as_non_null();
-        }
+		self.next = self.as_non_null();
+		self.prev = self.as_non_null();
+	}
 }
 
 impl<T> PartialEq for Node<T> {
-        fn eq(&self, other: &Self) -> bool {
-                self.as_ptr() == other.as_ptr()
-        }
+	fn eq(&self, other: &Self) -> bool {
+		self.as_ptr() == other.as_ptr()
+	}
 }
 
 pub struct NAList<T>{
@@ -77,113 +77,113 @@ pub struct NAList<T>{
 }
 
 impl<T> NAList<T> {
-	pub const fn new() -> Self {
-		NAList { head: None }
+pub const fn new() -> Self {
+	NAList { head: None }
+}
+
+pub fn count(&self) -> usize {
+	self.iter().count()
+}
+
+pub fn find_if<F>(&mut self, mut f: F) -> Option<NonNull<Node<T>>>
+where F: FnMut(&Node<T>) -> bool
+{
+	self.iter_mut().find(|n| f(n)).map(|n| n.as_non_null())
+}
+
+pub fn insert_front(&mut self, node: &mut Node<T>) {
+	if let None = self.head {
+		let node_ptr = node.as_non_null();
+		self.head = Some(node_ptr);
+		return;
 	}
 
-        pub fn count(&self) -> usize {
-                self.iter().count()
-        }
+	self.insert(node);
+	let node_ptr = node.as_non_null();
+	self.head = Some(node_ptr);
+}
 
-        pub fn find_if<F>(&mut self, mut f: F) -> Option<NonNull<Node<T>>>
-        where F: FnMut(&Node<T>) -> bool
-        {
-                self.iter_mut().find(|n| f(n)).map(|n| n.as_non_null())
-        }
-
-	pub fn insert_front(&mut self, node: &mut Node<T>) {
-                if let None = self.head {
-                        let node_ptr = node.as_non_null();
-                        self.head = Some(node_ptr);
-                        return;
-                }
-
-                self.insert(node);
-                let node_ptr = node.as_non_null();
-                self.head = Some(node_ptr);
+pub fn insert_back(&mut self, node: &mut Node<T>) {
+	if let None = self.head {
+		let node_ptr = node.as_non_null();
+		self.head = Some(node_ptr);
+		return;
 	}
 
-        pub fn insert_back(&mut self, node: &mut Node<T>) {
-                if let None = self.head {
-                        let node_ptr = node.as_non_null();
-                        self.head = Some(node_ptr);
-                        return;
-                }
-
-                self.insert(node);
-	}
+	self.insert(node);
+}
 
 
-        fn insert(&mut self, node: &mut Node<T>) {
-		let head = unsafe { self.head.unwrap().as_mut() };
-                let prev = unsafe { head.prev.as_mut() };
-                let node_ptr = node.as_non_null();
+fn insert(&mut self, node: &mut Node<T>) {
+	let head = unsafe { self.head.unwrap().as_mut() };
+	let prev = unsafe { head.prev.as_mut() };
+	let node_ptr = node.as_non_null();
 
-                prev.next = node_ptr;
-                head.prev = node_ptr;
+	prev.next = node_ptr;
+	head.prev = node_ptr;
 
-                node.next = head.as_non_null();
-                node.prev = prev.as_non_null();
-        }
-
-
-        pub fn remove_if<'page, F>(&mut self, f: F) -> Option<&'page mut Node<T>>
-        where F: FnMut(&Node<T>) -> bool
-        {
-                self.find_if(f).map(|mut node_ptr|{
-                        let node = unsafe { node_ptr.as_mut() };
-                        self.remove(node);
-                        node
-                })
-        }
-
-        fn remove(&mut self, node: &mut Node<T>) {
-                self.head.map(|mut head_ptr| {
-                        let head = unsafe { head_ptr.as_mut() };
-                        if node == head {
-                                self.head = Some(head.next);
-                        }
-
-                        if head_ptr == head.next {
-                                self.head = None;
-                        }
-
-                        node.disjoint()
-                });
-        }
+	node.next = head.as_non_null();
+	node.prev = prev.as_non_null();
+}
 
 
-        pub fn iter_mut(&mut self) -> IterMut<'_, T> {
-                IterMut::new(self)
-        }
+pub fn remove_if<'page, F>(&mut self, f: F) -> Option<&'page mut Node<T>>
+where F: FnMut(&Node<T>) -> bool
+{
+	self.find_if(f).map(|mut node_ptr|{
+		let node = unsafe { node_ptr.as_mut() };
+		self.remove(node);
+		node
+	})
+}
 
-        pub fn iter(&self) -> Iter<'_, T> {
-                Iter::new(self)
-        }
+fn remove(&mut self, node: &mut Node<T>) {
+	self.head.map(|mut head_ptr| {
+		let head = unsafe { head_ptr.as_mut() };
+		if node == head {
+			self.head = Some(head.next);
+		}
+
+		if head_ptr == head.next {
+			self.head = None;
+		}
+
+		node.disjoint()
+	});
+}
+
+
+pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+	IterMut::new(self)
+}
+
+pub fn iter(&self) -> Iter<'_, T> {
+	Iter::new(self)
+}
 }
 
 
 impl<T> Debug for NAList<T>
 where T: Debug
 {
-        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-                f.debug_list().entries(self).finish()
-        }
+fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+	f.debug_list().entries(self).finish()
+}
 }
 
 impl<'a, T> Extend<&'a mut Node<T>> for NAList<T> {
-        fn extend<I: IntoIterator<Item = &'a mut Node<T>>>(&mut self, iter: I) {
-                iter.into_iter().for_each(|n| {
-                        n.disjoint();
-                        self.insert(n);
-                })
-        }
+fn extend<I: IntoIterator<Item = &'a mut Node<T>>>(&mut self, iter: I) {
+	iter.into_iter().for_each(|n| {
+		n.disjoint();
+		self.insert(n);
+	})
+}
 }
 
 impl<T> Default for NAList<T> {
-        fn default() -> Self {
-                NAList::new()
-        }
+fn default() -> Self {
+	NAList::new()
+}
 }
 
 
--- src/mm/slub/size_cache/free_list/free_node.rs
+++ src/mm/slub/size_cache/free_list/free_node.rs
@@ -9,305 +9,305 @@ use super::FreeList;
 
 #[derive(Debug)]
 pub struct FreeNode {
-    pub prev: NonNull<FreeNode>,
-    pub next: NonNull<FreeNode>,
-    pub bytes: usize,
-    _pin: PhantomPinned
+	pub prev: NonNull<FreeNode>,
+	pub next: NonNull<FreeNode>,
+	pub bytes: usize,
+	_pin: PhantomPinned
 }
 
 impl PartialEq for FreeNode {
-        fn eq(&self, other: &Self) -> bool {
-                self.as_ptr() == other.as_ptr()
-        }
+	fn eq(&self, other: &Self) -> bool {
+		self.as_ptr() == other.as_ptr()
+	}
 }
 
 impl FreeNode {
-        pub const NODE_SIZE: usize = size_of::<FreeNode>();
+	pub const NODE_SIZE: usize = size_of::<FreeNode>();
 
-        /// Construct a FreeNode for memory chunk
-        /// 
-        /// # Safety
-        /// 
-        /// * The size of memory chunk must be bigger than FreeNode::NODE_SIZE
+	/// Construct a FreeNode for memory chunk
+	/// 
+	/// # Safety
+	/// 
+	/// * The size of memory chunk must be bigger than FreeNode::NODE_SIZE
 	pub unsafe fn construct_at(mem: &mut [u8]) -> &mut Self {
-                let bytes = mem.len();
-                let mem = mem.as_mut_ptr() as *mut Self;
-                
-                let next = NonNull::new_unchecked(&mut (*mem));
-                let prev = next.clone();
-                (*mem) = FreeNode { prev, next, bytes, _pin: PhantomPinned };
-                &mut (*mem)
+		let bytes = mem.len();
+		let mem = mem.as_mut_ptr() as *mut Self;
+		
+		let next = NonNull::new_unchecked(&mut (*mem));
+		let prev = next.clone();
+		(*mem) = FreeNode { prev, next, bytes, _pin: PhantomPinned };
+		&mut (*mem)
 	}
 
-        /// Try to merge `self` with previous and next node.
-        /// If `self` doesn't have previous or next node, this function do nothing.
-        pub fn try_merge(&mut self) {
-                let (front, back) = unsafe {
-                        (self.prev.as_mut(), self.next.as_mut())
-                };
+	/// Try to merge `self` with previous and next node.
+	/// If `self` doesn't have previous or next node, this function do nothing.
+	pub fn try_merge(&mut self) {
+		let (front, back) = unsafe {
+			(self.prev.as_mut(), self.next.as_mut())
+		};
 
-                let self_start = self.as_ptr().cast::<u8>();
-                let back_start = back.as_ptr().cast::<u8>();
-                let front_start = front.as_ptr().cast::<u8>();
+		let self_start = self.as_ptr().cast::<u8>();
+		let back_start = back.as_ptr().cast::<u8>();
+		let front_start = front.as_ptr().cast::<u8>();
 
-                let self_end = unsafe { self_start.offset(self.bytes as isize) };
-                let front_end = unsafe { front_start.offset(front.bytes as isize) };
+		let self_end = unsafe { self_start.offset(self.bytes as isize) };
+		let front_end = unsafe { front_start.offset(front.bytes as isize) };
 
-                if self_end == back_start  {
-                        let next = unsafe { back.next.as_mut() };
-                        next.prev = self.as_non_null();
-                        self.next = back.next;
-                        self.bytes += back.bytes;
-                }
+		if self_end == back_start  {
+			let next = unsafe { back.next.as_mut() };
+			next.prev = self.as_non_null();
+			self.next = back.next;
+			self.bytes += back.bytes;
+		}
 
-                if front_end == self_start  {
-                        let back = unsafe { self.next.as_mut() };
-                        back.prev = self.prev;
-                        front.next = self.next;
-                        front.bytes += self.bytes;
-                }
-        }
+		if front_end == self_start  {
+			let back = unsafe { self.next.as_mut() };
+			back.prev = self.prev;
+			front.next = self.next;
+			front.bytes += self.bytes;
+		}
+	}
 
-        pub fn disjoint(&mut self) {
-                let prev = unsafe { self.prev.as_mut() };
-                let next = unsafe { self.next.as_mut() };
+	pub fn disjoint(&mut self) {
+		let prev = unsafe { self.prev.as_mut() };
+		let next = unsafe { self.next.as_mut() };
 
-                prev.next = self.next;
-                next.prev = self.prev;
+		prev.next = self.next;
+		next.prev = self.prev;
 
-                self.next = self.as_non_null();
-                self.prev = self.as_non_null();
-        }
+		self.next = self.as_non_null();
+		self.prev = self.as_non_null();
+	}
 
-        pub fn contains<T>(&self, ptr: NonNull<T>) -> bool {
-                let ptr = ptr.as_ptr() as usize;
-                let s = self.addr().cast::<u8>() as usize;
-                
-                match s.checked_add(self.bytes) {
-                        Some(e) => s <= ptr && ptr < e,
-                        None => s <= ptr && ptr <= usize::MAX
-                }
-        }
+	pub fn contains<T>(&self, ptr: NonNull<T>) -> bool {
+		let ptr = ptr.as_ptr() as usize;
+		let s = self.addr().cast::<u8>() as usize;
+		
+		match s.checked_add(self.bytes) {
+			Some(e) => s <= ptr && ptr < e,
+			None => s <= ptr && ptr <= usize::MAX
+		}
+	}
 
-        /// It is called by `CacheManager` for collecting excess cache memory allcated by `PAGE_ALLOC`.
-        ///
-        /// `'=': inuse, '-': free`
-        /// 
-        /// * case 0)
-        /// ```
-        /// align0     align1
-        /// start       end
-        ///   |----((----|
-        ///   |----))----|
-        ///
-        /// ```
-        /// * case 1)
-        /// ```
-        /// align0
-        /// start    align1   end
-        ///   |---((---|-------|
-        ///   |---))---|-------|
-        ///
-        /// ```
-        /// * case 2)
-        /// ```
-        ///                          align2
-        /// align0  start   align1    end
-        ///   |=======|-------|---((---|
-        ///   |=======|-------|---))---|
-        ///   
-        /// ```
-        /// * case 3)
-        /// ```
-        /// align0  start   align1   align2   end
-        ///   |=======|-------|---((---|-------|
-        ///   |=======|-------|---))---|-------|
-        /// ```
-        ///
-        pub fn shrink(&mut self, free_list: &mut FreeList, rank: usize) -> (NonNull<u8>, usize) {
-                let align = align_of_rank(rank);
-                let size = size_of_rank(rank);
+	/// It is called by `CacheManager` for collecting excess cache memory allcated by `PAGE_ALLOC`.
+	///
+	/// `'=': inuse, '-': free`
+	/// 
+	/// * case 0)
+	/// ```
+	/// align0     align1
+	/// start       end
+	///   |----((----|
+	///   |----))----|
+	///
+	/// ```
+	/// * case 1)
+	/// ```
+	/// align0
+	/// start    align1   end
+	///   |---((---|-------|
+	///   |---))---|-------|
+	///
+	/// ```
+	/// * case 2)
+	/// ```
+	///                          align2
+	/// align0  start   align1    end
+	///   |=======|-------|---((---|
+	///   |=======|-------|---))---|
+	///   
+	/// ```
+	/// * case 3)
+	/// ```
+	/// align0  start   align1   align2   end
+	///   |=======|-------|---((---|-------|
+	///   |=======|-------|---))---|-------|
+	/// ```
+	///
+	pub fn shrink(&mut self, free_list: &mut FreeList, rank: usize) -> (NonNull<u8>, usize) {
+		let align = align_of_rank(rank);
+		let size = size_of_rank(rank);
 
-                let mut total = self.bytes;
-                let start = self.as_mut_ptr().cast::<u8>() as usize;
-                let end = match start.checked_add(total) {
-                        Some(e) => e,
-                        None => 0,
-                };
-                let next_align = next_align(start, align);
+		let mut total = self.bytes;
+		let start = self.as_mut_ptr().cast::<u8>() as usize;
+		let end = match start.checked_add(total) {
+			Some(e) => e,
+			None => 0,
+		};
+		let next_align = next_align(start, align);
 
-                if !is_aligned(end, align) {
-                        let len = end - prev_align(end, align);
-                        let n = unsafe {
-                                FreeNode::construct_at(
-                                        slice::from_raw_parts_mut(end as *mut u8, len)
-                                )
-                        };
-                        total -= n.bytes;
-                        free_list.insert(n);
-                }
+		if !is_aligned(end, align) {
+			let len = end - prev_align(end, align);
+			let n = unsafe {
+				FreeNode::construct_at(
+					slice::from_raw_parts_mut(end as *mut u8, len)
+				)
+			};
+			total -= n.bytes;
+			free_list.insert(n);
+		}
 
-                if !is_aligned(start, align) {
-                        self.bytes = next_align - start;
-                        total -= self.bytes;
-                        free_list.insert(self);
-                }
+		if !is_aligned(start, align) {
+			self.bytes = next_align - start;
+			total -= self.bytes;
+			free_list.insert(self);
+		}
 
-                (unsafe { NonNull::new_unchecked(next_align as *mut u8) }, total / size)
-        }
+		(unsafe { NonNull::new_unchecked(next_align as *mut u8) }, total / size)
+	}
 
-        #[inline(always)]
-        pub fn bytes(&self) -> usize {
-                self.bytes
-        }
+	#[inline(always)]
+	pub fn bytes(&self) -> usize {
+		self.bytes
+	}
 
-        #[inline(always)]
-        pub fn as_mut_ptr(&mut self) -> *mut Self {
-                self as *mut Self
-        }
+	#[inline(always)]
+	pub fn as_mut_ptr(&mut self) -> *mut Self {
+		self as *mut Self
+	}
 
-        #[inline(always)]
-        pub fn as_ptr(&self) -> *const Self {
-                self as *const Self
-        }
+	#[inline(always)]
+	pub fn as_ptr(&self) -> *const Self {
+		self as *const Self
+	}
 
-        #[inline(always)]
-        pub fn as_non_null(&mut self) -> NonNull<Self> {
-                unsafe { NonNull::new_unchecked(self.as_mut_ptr()) }
-        }
+	#[inline(always)]
+	pub fn as_non_null(&mut self) -> NonNull<Self> {
+		unsafe { NonNull::new_unchecked(self.as_mut_ptr()) }
+	}
 
-        #[inline(always)]
-        pub fn addr(&self) -> *const FreeNode {
-                self as *const FreeNode
-        }
+	#[inline(always)]
+	pub fn addr(&self) -> *const FreeNode {
+		self as *const FreeNode
+	}
 }
 
 pub(super) mod node_tests {
 
-        use kfs_macro::ktest;
+	use kfs_macro::ktest;
 
-        use super::*;
+	use super::*;
 
-        const PAGE_SIZE: usize = 4096;
-        static mut PAGE1: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
+	const PAGE_SIZE: usize = 4096;
+	static mut PAGE1: [u8; PAGE_SIZE] = [0; PAGE_SIZE];
 
-        pub fn new_node(page: &mut [u8], offset:usize, bytes: usize) -> &mut FreeNode {
-                let ptr = unsafe { (page as *mut [u8] as *mut u8).offset(offset as isize) };
-                let mem = unsafe { core::slice::from_raw_parts_mut(ptr, bytes) };
-                unsafe { FreeNode::construct_at(mem) }
-        }
+	pub fn new_node(page: &mut [u8], offset:usize, bytes: usize) -> &mut FreeNode {
+		let ptr = unsafe { (page as *mut [u8] as *mut u8).offset(offset as isize) };
+		let mem = unsafe { core::slice::from_raw_parts_mut(ptr, bytes) };
+		unsafe { FreeNode::construct_at(mem) }
+	}
 
-        #[ktest]
-        fn test_construct_at() {
-                let page = unsafe { &mut PAGE1 };
-                let page_ptr = page.as_mut_ptr() as *mut FreeNode;
-                let node = unsafe { FreeNode::construct_at(page) };
+	#[ktest]
+	fn test_construct_at() {
+		let page = unsafe { &mut PAGE1 };
+		let page_ptr = page.as_mut_ptr() as *mut FreeNode;
+		let node = unsafe { FreeNode::construct_at(page) };
 
-                assert_eq!(node.next.as_ptr(), page_ptr);
-                assert_eq!(node.prev.as_ptr(), page_ptr);
-                assert_eq!(node.as_ptr(), page_ptr);
-        }
+		assert_eq!(node.next.as_ptr(), page_ptr);
+		assert_eq!(node.prev.as_ptr(), page_ptr);
+		assert_eq!(node.as_ptr(), page_ptr);
+	}
 
-        #[ktest]
-        fn test_try_merge() {
+	#[ktest]
+	fn test_try_merge() {
 
-                fn init_nodes<'a>() -> (&'a mut FreeNode, &'a mut FreeNode) {
-                        let page = unsafe { &mut PAGE1 };
+		fn init_nodes<'a>() -> (&'a mut FreeNode, &'a mut FreeNode) {
+			let page = unsafe { &mut PAGE1 };
 
-                        let node1 = new_node(page, 30, 30);
-                        let mut node1_ptr = node1.as_non_null();
-                        let node1 = unsafe { node1_ptr.as_mut() };
+			let node1 = new_node(page, 30, 30);
+			let mut node1_ptr = node1.as_non_null();
+			let node1 = unsafe { node1_ptr.as_mut() };
 
-                        let node0 = new_node(page, 0, 30);
-                        let node0_ptr = node0.as_non_null();
+			let node0 = new_node(page, 0, 30);
+			let node0_ptr = node0.as_non_null();
 
-                        node1.prev = node0_ptr;
-                        node1.next = node0_ptr;
-                        node0.prev = node1_ptr;
-                        node0.next = node1_ptr;
-                        (node0, node1)
-                }
-                
-                // merge with next node.
-                let (node0, _) = init_nodes();
-                let node0_ptr = node0.as_non_null();
-                node0.try_merge();
-                assert_eq!(node0.bytes, 60);
-                assert_eq!(node0.prev, node0_ptr);
-                assert_eq!(node0.next, node0_ptr);
+			node1.prev = node0_ptr;
+			node1.next = node0_ptr;
+			node0.prev = node1_ptr;
+			node0.next = node1_ptr;
+			(node0, node1)
+		}
+		
+		// merge with next node.
+		let (node0, _) = init_nodes();
+		let node0_ptr = node0.as_non_null();
+		node0.try_merge();
+		assert_eq!(node0.bytes, 60);
+		assert_eq!(node0.prev, node0_ptr);
+		assert_eq!(node0.next, node0_ptr);
 
-                // merge with previous node.
-                let (node0, node1) = init_nodes();
-                let node0_ptr = node0.as_non_null();
-                node1.try_merge();
-                assert_eq!(node0.bytes, 60);
-                assert_eq!(node0.prev, node0_ptr);
-                assert_eq!(node0.next, node0_ptr);
+		// merge with previous node.
+		let (node0, node1) = init_nodes();
+		let node0_ptr = node0.as_non_null();
+		node1.try_merge();
+		assert_eq!(node0.bytes, 60);
+		assert_eq!(node0.prev, node0_ptr);
+		assert_eq!(node0.next, node0_ptr);
 
-                // Never merge.
-                let node2 = new_node(unsafe { &mut PAGE1 }, 300, 30);
-                let node2_ptr = node2.as_non_null();
-                node2.try_merge();
-                assert_eq!(node0.bytes, 60);
-                assert_eq!(node0.prev, node0_ptr);
-                assert_eq!(node0.next, node0_ptr);
-                assert_eq!(node2.bytes, 30);
-                assert_eq!(node2.prev, node2_ptr);
-                assert_eq!(node2.next, node2_ptr);
-        }
-        
-        #[ktest]
-        fn test_disjoint() {
+		// Never merge.
+		let node2 = new_node(unsafe { &mut PAGE1 }, 300, 30);
+		let node2_ptr = node2.as_non_null();
+		node2.try_merge();
+		assert_eq!(node0.bytes, 60);
+		assert_eq!(node0.prev, node0_ptr);
+		assert_eq!(node0.next, node0_ptr);
+		assert_eq!(node2.bytes, 30);
+		assert_eq!(node2.prev, node2_ptr);
+		assert_eq!(node2.next, node2_ptr);
+	}
 
-                fn do_test(node: &mut FreeNode, left: &FreeNode, right: &FreeNode) {
-                        let node_ptr = node.as_non_null();
-                        node.disjoint();
-                        assert_eq!(node.prev, node_ptr);
-                        assert_eq!(node.next, node_ptr);
-        
-                        assert_ne!(left.next, node_ptr);
-                        assert_ne!(right.prev, node_ptr);
-                }
-        
-                fn make_3nodes_jointed<'a>() -> (&'a mut FreeNode, &'a mut FreeNode, &'a mut FreeNode) {
-                        let page = unsafe { &mut PAGE1 };
-        
-                        let node0 = new_node(page, 0, 31);
-                        let mut node0_ptr = node0.as_non_null();
-                        let node0 = unsafe { node0_ptr.as_mut() };
-        
-                        let node1 = new_node(page, 100, 32);
-                        let mut node1_ptr = node1.as_non_null();
-                        let node1 = unsafe { node1_ptr.as_mut() };
-        
-                        let node2 = new_node(page, 200, 33);
-                        let mut node2_ptr = node2.as_non_null();
-                        let node2 = unsafe { node2_ptr.as_mut() };
-        
-                        node0.prev = node2_ptr;
-                        node0.next = node1_ptr;
-        
-                        node1.prev = node0_ptr;
-                        node1.next = node2_ptr;
-        
-                        node2.prev = node1_ptr;
-                        node2.next = node0_ptr;
-        
-                        (node0, node1, node2)
-                }
-        
+	#[ktest]
+	fn test_disjoint() {
 
-                // disjoint first
-                let nodes = make_3nodes_jointed();
-                do_test(nodes.0, nodes.2, nodes.1);
+		fn do_test(node: &mut FreeNode, left: &FreeNode, right: &FreeNode) {
+			let node_ptr = node.as_non_null();
+			node.disjoint();
+			assert_eq!(node.prev, node_ptr);
+			assert_eq!(node.next, node_ptr);
 
-                // disjoint second
-                let nodes = make_3nodes_jointed();
-                do_test(nodes.1, nodes.0, nodes.2);
+			assert_ne!(left.next, node_ptr);
+			assert_ne!(right.prev, node_ptr);
+		}
 
-                // disjoint last
-                let nodes = make_3nodes_jointed();
-                do_test(nodes.2, nodes.1, nodes.0);
-        }
+		fn make_3nodes_jointed<'a>() -> (&'a mut FreeNode, &'a mut FreeNode, &'a mut FreeNode) {
+			let page = unsafe { &mut PAGE1 };
+
+			let node0 = new_node(page, 0, 31);
+			let mut node0_ptr = node0.as_non_null();
+			let node0 = unsafe { node0_ptr.as_mut() };
+
+			let node1 = new_node(page, 100, 32);
+			let mut node1_ptr = node1.as_non_null();
+			let node1 = unsafe { node1_ptr.as_mut() };
+
+			let node2 = new_node(page, 200, 33);
+			let mut node2_ptr = node2.as_non_null();
+			let node2 = unsafe { node2_ptr.as_mut() };
+
+			node0.prev = node2_ptr;
+			node0.next = node1_ptr;
+
+			node1.prev = node0_ptr;
+			node1.next = node2_ptr;
+
+			node2.prev = node1_ptr;
+			node2.next = node0_ptr;
+
+			(node0, node1, node2)
+		}
+
+
+		// disjoint first
+		let nodes = make_3nodes_jointed();
+		do_test(nodes.0, nodes.2, nodes.1);
+
+		// disjoint second
+		let nodes = make_3nodes_jointed();
+		do_test(nodes.1, nodes.0, nodes.2);
+
+		// disjoint last
+		let nodes = make_3nodes_jointed();
+		do_test(nodes.2, nodes.1, nodes.0);
+	}
 }
 
