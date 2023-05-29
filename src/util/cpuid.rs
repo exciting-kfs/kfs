3738a3792d949fe308c072e26c9f6e796367c2fe
42mypark <skyttl34@gmail.com>
2023-05-29 15:34:26 +0900

    feat: msr

---
+++ src/util/cpuid.rs
@@ -0,0 +1,51 @@
+#[derive(Debug, PartialEq)]
+pub struct CPUID {
+	eax: usize,
+	ebx: usize,
+	ecx: usize,
+	edx: usize,
+}
+
+impl CPUID {
+	fn run(mut leaf: usize, mut sub_leaf: usize) -> Self {
+		let ebx;
+		let edx;
+
+		unsafe {
+			core::arch::asm!(
+				"cpuid", inout("eax") leaf, out("ebx") ebx, inout("ecx") sub_leaf, out("edx") edx
+			);
+		}
+
+		CPUID {
+			eax: leaf,
+			ebx,
+			ecx: sub_leaf,
+			edx,
+		}
+	}
+}
+
+mod cpuid_test {
+	use crate::pr_info;
+
+	use super::*;
+	use kfs_macro::ktest;
+
+	#[ktest]
+	fn cpuid_test() {
+		// assert_eq!(
+		// 	CPUID::run(0, 0),
+		// 	CPUID {
+		// 		eax: 0x4,
+		// 		ebx: 0x756e6547,
+		// 		ecx: 0x6c65746e,
+		// 		edx: 0x49656e69
+		// 	}
+		// );
+
+		pr_info!("{:x?}", CPUID::run(0, 0));
+		pr_info!("{:x?}", CPUID::run(1, 0));
+		pr_info!("{:x?}", CPUID::run(0x80000008, 0));
+	}
+}
