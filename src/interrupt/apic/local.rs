2cfa95cd45e022e742a3b161c70caddfa77d14e9
42mypark <skyttl34@gmail.com>
2023-06-13 15:49:22 +0900

    fix: lapic register size. 4->8

--- src/interrupt/apic/local.rs
+++ src/interrupt/apic/local.rs
@@ -25,14 +25,26 @@ pub enum Register {
 	InService1 = 0x110,
 	InService2 = 0x120,
 	InService3 = 0x130,
+	InService4 = 0x140,
+	InService5 = 0x150,
+	InService6 = 0x160,
+	InService7 = 0x170,
 	TriggerMode0 = 0x180,
 	TriggerMode1 = 0x190,
 	TriggerMode2 = 0x1a0,
 	TriggerMode3 = 0x1b0,
+	TriggerMode4 = 0x1c0,
+	TriggerMode5 = 0x1d0,
+	TriggerMode6 = 0x1e0,
+	TriggerMode7 = 0x1f0,
 	InterruptRequest0 = 0x200,
 	InterruptRequest1 = 0x210,
 	InterruptRequest2 = 0x220,
 	InterruptRequest3 = 0x230,
+	InterruptRequest4 = 0x240,
+	InterruptRequest5 = 0x250,
+	InterruptRequest6 = 0x260,
+	InterruptRequest7 = 0x270,
 	ErrorStatus = 0x280,
 	CorrectedMachineCheckInterrupt = 0x2f0,
 	InterruptCommand0 = 0x300,
@@ -64,13 +76,12 @@ impl Register {
 	}
 
 	pub fn iter() -> core::slice::Iter<'static, Register> {
-		const REGISTERS: [Register; 35] = [
+		const REGISTERS: [Register; 45] = [
 			Register::ID,
 			Register::Version,
 			Register::TaskPriorty,
 			Register::ArbitrationPriority,
 			Register::ProcessorPriority,
-			Register::EndOfInterrupt,
 			Register::RemoteRead,
 			Register::LogicalDestination,
 			Register::DestinationFormat,
@@ -79,16 +90,28 @@ impl Register {
 			Register::InService1,
 			Register::InService2,
 			Register::InService3,
+			Register::InService4,
+			Register::InService5,
+			Register::InService6,
+			Register::InService7,
 			Register::TriggerMode0,
 			Register::TriggerMode1,
 			Register::TriggerMode2,
 			Register::TriggerMode3,
+			Register::TriggerMode4,
+			Register::TriggerMode5,
+			Register::TriggerMode6,
+			Register::TriggerMode7,
 			Register::InterruptRequest0,
 			Register::InterruptRequest1,
 			Register::InterruptRequest2,
 			Register::InterruptRequest3,
+			Register::InterruptRequest4,
+			Register::InterruptRequest5,
+			Register::InterruptRequest6,
+			Register::InterruptRequest7,
 			Register::ErrorStatus,
-			Register::CorrectedMachineCheckInterrupt,
+			// Register::CorrectedMachineCheckInterrupt,
 			Register::InterruptCommand0,
 			Register::InterruptCommand1,
 			Register::LvtTimer,
@@ -120,6 +143,10 @@ impl core::fmt::Display for Register {
 			Self::InService1 => "ISR1",
 			Self::InService2 => "ISR2",
 			Self::InService3 => "ISR3",
+			Self::InService4 => "ISR4",
+			Self::InService5 => "ISR5",
+			Self::InService6 => "ISR6",
+			Self::InService7 => "ISR7",
 			Self::InitialCount => "InitialCount",
 			Self::InterruptCommand0 => "ICR0",
 			Self::InterruptCommand1 => "ICR1",
@@ -127,6 +154,10 @@ impl core::fmt::Display for Register {
 			Self::InterruptRequest1 => "IRR1",
 			Self::InterruptRequest2 => "IRR2",
 			Self::InterruptRequest3 => "IRR3",
+			Self::InterruptRequest4 => "IRR4",
+			Self::InterruptRequest5 => "IRR5",
+			Self::InterruptRequest6 => "IRR6",
+			Self::InterruptRequest7 => "IRR7",
 			Self::LogicalDestination => "LDR",
 			Self::LvtError => "LVT Error",
 			Self::LvtLint0 => "LVT LINT0",
@@ -142,6 +173,10 @@ impl core::fmt::Display for Register {
 			Self::TriggerMode1 => "TMR1",
 			Self::TriggerMode2 => "TMR2",
 			Self::TriggerMode3 => "TMR3",
+			Self::TriggerMode4 => "TMR4",
+			Self::TriggerMode5 => "TMR5",
+			Self::TriggerMode6 => "TMR6",
+			Self::TriggerMode7 => "TMR7",
 			Self::Version => "Version",
 		};
 		write!(f, "{}", s)
@@ -156,11 +191,11 @@ fn read_n<const N: usize>(init_register: Register, buf: &mut [usize; N]) {
 	}
 }
 
-pub fn read_in_service(buf: &mut [usize; 4]) {
+pub fn read_in_service(buf: &mut [usize; 8]) {
 	read_n(Register::InService0, buf)
 }
 
-pub fn read_interrupt_request(buf: &mut [usize; 4]) {
+pub fn read_interrupt_request(buf: &mut [usize; 8]) {
 	read_n(Register::InterruptRequest0, buf)
 }
 
@@ -169,8 +204,8 @@ pub fn read_interrupt_command(buf: &mut [usize; 2]) {
 }
 
 pub fn write_interrupt_command(value: &[usize; 2]) {
-	let addr = Register::InterruptCommand0.addr();
-	value.iter().enumerate().for_each(|(i, v)| {
+	let addr = Register::InterruptCommand1.addr();
+	value.iter().rev().enumerate().for_each(|(i, v)| {
 		let ptr = (addr + i * 0x10) as *mut usize;
 		unsafe { *ptr = *v };
 	});
