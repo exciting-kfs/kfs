0233110a199c561e66eb3e22301845fb8ce603d5
cjeon <cjeon@student.42seoul.kr>
2023-08-06 13:57:47 +0900

    Update init.c
    remove comment

--- userspace/init/init.c
+++ userspace/init/init.c
@@ -6,7 +6,6 @@ int main(void) {
 	if (pid == 0) {
 		void *p = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MMAP_PRIVATE, -1, 0);
 
-		// *(unsigned char *)p = 42;
 		*(unsigned char *)p = 42;
 		exec("test_sig_stop_cont.bin");
 		_exit(1);
