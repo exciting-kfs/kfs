7abfe838f246794242c50695f1bf8d951f73b0fa
mypark <skyttl34@gmail.com>
2023-08-30 18:30:02 +0900

    fix kthread (#173)

--- src/process/kthread.rs
+++ src/process/kthread.rs
@@ -1,9 +1,10 @@
+use crate::interrupt::irq_enable;
+
 use super::exit::sys_exit;
 
 /// Re-enable IRQ and execute thread routine
 pub extern "C" fn kthread_entry(routine: extern "C" fn(usize) -> usize, arg: usize) {
-	// FIXME: irq
-	// context_switch(InContext::Kernel);
+	irq_enable();
 	let ret = routine(arg);
 	sys_exit(ret);
 }
