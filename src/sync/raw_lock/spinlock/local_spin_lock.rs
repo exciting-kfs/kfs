cfe95ae1f50db809f89ab5a183b6364149e1e252
42mypark <skyttl34@gmail.com>
2023-09-22 19:28:40 +0900

    fix: check condition

--- src/sync/raw_lock/spinlock/local_spin_lock.rs
+++ src/sync/raw_lock/spinlock/local_spin_lock.rs
@@ -1,10 +1,10 @@
+use core::sync::atomic::Ordering;
+
 use crate::interrupt::in_interrupt_context;
-use crate::interrupt::irq_disable;
-use crate::interrupt::irq_enable;
 use crate::process::signal::poll_signal_queue;
 use crate::scheduler::context::yield_now;
-use crate::sync::raw_lock::spinlock::get_lock_depth;
 use crate::syscall::errno::Errno;
+use crate::RUN_TIME;
 
 use super::RawSpinLock;
 use super::TryLockFail;
@@ -54,12 +54,10 @@ impl LocalSpinLock {
 }
 
 fn check_precondition() {
-	if cfg!(dbg) {
-		irq_disable();
-		assert!(
-			!in_interrupt_context() && get_lock_depth() == 0,
+	if RUN_TIME.load(Ordering::Relaxed) {
+		debug_assert!(
+			!in_interrupt_context(),
 			"Cannot use LocalSpinLock when `yield_now` is impossible or in interrupt context."
 		);
-		irq_enable();
 	}
 }
