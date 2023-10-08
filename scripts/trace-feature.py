dcf0eaf0662e2e0000dd5d8f5fa4e7bbc405be5c
42mypark <skyttl34@gmail.com>
2023-10-08 09:11:08 +0900

    fix: '|' grammar

--- scripts/trace-feature.py
+++ scripts/trace-feature.py
@@ -4,19 +4,21 @@ import re
 # Define the project directory
 project_directory = './'
 
-# Regular expression pattern to match 'trace_feature!("feat-name", .*)' with optional additional arguments
-pattern = r'trace_feature!\s*\(\s*("[^"]+").*\)'
+# Regular expression pattern to match 'trace_feature' lines and extract feature names
+pattern = r'trace_feature!\s*\(\s*("[^"]+"\s*(?:\|\s*"[^"]+")*)\s*,\s*.*\)'
 
-# Function to search for and extract feature names
+# Function to search for 'trace_feature' lines and extract feature names
 def extract_feature_names(file_path):
     feature_names = []
     with open(file_path, 'r') as file:
         content = file.read()
         matches = re.findall(pattern, content)
-        feature_names.extend(matches)
+        for match in matches:
+            names = re.findall(r'"([^"]+)"', match)
+            feature_names.extend(names)
     return feature_names
 
-# Function to search for 'trace_feature!("feat-name", .*)' in Rust files
+# Function to search for 'trace_feature' in Rust files
 def search_for_trace_feature(directory):
     feature_names = []
     for root, _, files in os.walk(directory):
@@ -26,10 +28,10 @@ def search_for_trace_feature(directory):
                 feature_names.extend(extract_feature_names(file_path))
     return feature_names
 
-# Call the function to search for 'trace_feature!("feat-name", .*)' in Rust files and print the extracted feature names
+# Call the function to search for 'trace_feature' in Rust files and print the extracted feature names
 feature_names = search_for_trace_feature(project_directory)
-unique_feature_names = set(feature_names)
+feature_set = set(feature_names)
 
-print('[Trace Feature List]:')
-for name in unique_feature_names:
+print("[Trace fature list]")
+for name in sorted(feature_set):
     print(name.strip('"'))
