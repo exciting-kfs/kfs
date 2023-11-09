d53699f8f8dc8a3f5f7054e53f370ba1017e61a0
cjeon <cjeon@student.42seoul.kr>
2023-11-09 15:10:31 +0900

    fix: do not copy

--- src/syscall/uname.rs
+++ src/syscall/uname.rs
@@ -1,4 +1,4 @@
-use core::{mem, ptr::addr_of_mut};
+use core::ptr::addr_of_mut;
 
 use crate::{mm::user::verify::verify_ptr_mut, process::task::CURRENT};
 
@@ -27,18 +27,14 @@ pub fn sys_uname(uname_buf: usize) -> Result<usize, Errno> {
 
 	let buf = verify_ptr_mut::<UnameBuf>(uname_buf, current)?;
 
-	let mut src: UnameBuf = unsafe { mem::zeroed() };
-
 	unsafe {
-		write_field!(src.sysname, b"KFS\0");
-		write_field!(src.nodename, b"kfs\0");
-		write_field!(src.release, b"v0.0.9\0");
-		write_field!(src.version, b"default\0");
-		write_field!(src.machine, b"x86\0");
-		write_field!(src.domainname, b"(none)\0");
+		write_field!(buf.sysname, b"KFS\0");
+		write_field!(buf.nodename, b"kfs\0");
+		write_field!(buf.release, b"v0.0.9\0");
+		write_field!(buf.version, b"default\0");
+		write_field!(buf.machine, b"x86\0");
+		write_field!(buf.domainname, b"(none)\0");
 	}
-
-	*buf = src;
 
 	Ok(0)
 }
