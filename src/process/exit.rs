9a8ae2d046cf2be2493b6745ce816529bf3e5069
cjeon <cjeon@student.42seoul.kr>
2023-11-16 10:41:49 +0900

    wait: proper abi (#262)

--- src/process/exit.rs
+++ src/process/exit.rs
@@ -10,18 +10,16 @@ pub struct ExitStatus {
 	raw: usize,
 }
 
-#[repr(usize)]
-pub enum ExitFlag {
-	Signaled = 0x0100_0000,
-	Stopped = 0x0200_0000,
-	Exited = 0x0300_0000,
-	CoreDumped = 0x0400_0000,
-}
-
 impl ExitStatus {
-	pub fn new(flag: ExitFlag, status: u8) -> Self {
+	pub fn new_signaled(termsig: u8) -> Self {
 		Self {
-			raw: flag as usize | status as usize,
+			raw: (termsig & 0x7f) as usize,
+		}
+	}
+
+	pub fn new_exited(status: u8) -> Self {
+		Self {
+			raw: (status as usize) << 8,
 		}
 	}
 
@@ -32,7 +30,7 @@ impl ExitStatus {
 
 pub fn sys_exit(status: usize) -> ! {
 	let current = unsafe { CURRENT.get_mut() };
-	current.exit(ExitStatus::new(ExitFlag::Exited, status as u8));
+	current.exit(ExitStatus::new_exited(status as u8));
 
 	yield_now();
 	unreachable!("cannot scheduled after sys_exit");
@@ -42,7 +40,7 @@ pub fn exit_with_signal(sig: SigNum) -> ! {
 	let current = unsafe { CURRENT.get_mut() };
 
 	pr_debug!("{:?} exit with SIG{:?}", current.get_pid(), sig);
-	current.exit(ExitStatus::new(ExitFlag::Signaled, sig as usize as u8));
+	current.exit(ExitStatus::new_signaled(sig as usize as u8));
 
 	yield_now();
 	unreachable!("cannot scheduled after exit_with_signal");
